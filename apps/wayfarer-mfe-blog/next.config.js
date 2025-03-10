116a5be97f798cef03e464a82a2a5cb3b794bc12
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2025-03-11 00:30:17 +0530

    Configure env variables

--- apps/wayfarer-mfe-blog/next.config.js
+++ apps/wayfarer-mfe-blog/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
 };
 
 export default nextConfig;
--- apps/wayfarer-mfe-home/next.config.js
+++ apps/wayfarer-mfe-home/next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+  assetPrefix: process.env.NEXT_PUBLIC_HOME_URL || "",
+
   async rewrites() {
     return [
       {
         source: "/explore/:path*",
-        destination: "http://localhost:3001/:path*",
+        destination: `${process.env.NEXT_PUBLIC_CATALOG_URL}/:path*`,
       },
       {
         source: "/blog/:path*",
-        destination: "http://localhost:3002/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/:path*`,
       },
     ];
   },
