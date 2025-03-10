125d41a325ac4740e99e6750515c868e861d7e63
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2025-03-11 00:20:05 +0530

    Setup Redirects

--- apps/wayfarer-mfe-catalog/next.config.js
+++ apps/wayfarer-mfe-catalog/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/explore",
 };
 
 export default nextConfig;
--- apps/wayfarer-mfe-home/next.config.js
+++ apps/wayfarer-mfe-home/next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
     return [
       {
         source: "/explore/:path*",
-        destination: "http://localhost:3001/explore/:path*",
+        destination: "http://localhost:3001/:path*",
       },
       {
         source: "/blog/:path*",
-        destination: "http://localhost:3002/blog/:path*",
+        destination: "http://localhost:3002/:path*",
       },
     ];
   },
