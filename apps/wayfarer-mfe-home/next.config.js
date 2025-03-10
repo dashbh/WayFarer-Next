9ee593562b760aada10756775328168585d73506
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2025-03-11 01:34:39 +0530

    Deployment in vercel #2

--- apps/wayfarer-mfe-home/next.config.js
+++ apps/wayfarer-mfe-home/next.config.js
@@ -3,18 +3,18 @@ const nextConfig = {
   output: "standalone",
   assetPrefix: process.env.NEXT_PUBLIC_HOME_URL || "",
 
-  async rewrites() {
-    return [
-      {
-        source: "/explore/:path*",
-        destination: `${process.env.NEXT_PUBLIC_CATALOG_URL}/:path*`,
-      },
-      {
-        source: "/blog/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/explore/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_CATALOG_URL}/:path*`,
+  //     },
+  //     {
+  //       source: "/blog/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_BLOG_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
